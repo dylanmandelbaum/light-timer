c66c2dbebb73ff5f54a8a9e19f241ebd75182d67
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-17 13:02:01 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,13 @@
-const CACHE_NAME = "light-timer-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
+const CACHE_NAME = "light-timer-v2";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(fetch(event.request));
 });
