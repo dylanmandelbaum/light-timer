da729dec01c903eb5e7a0a802762113422a55e9b
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-19 23:53:46 -0700

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,27 +1,13 @@
 const PROFILES = {
 
-    on_off: {
+    get_ready:{name: "Get Ready", sequence: [0,5]},
+    rest:{name: "Rest", sequence: [0,10]},
+    complete:{name: "Complete!", sequence: [0,3]},
+    gentle_pulses:{name: "Gentle Pulses", sequence: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
+    gentle_squeeze:{name: "Gentle Pulses", sequence: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]},
+    quick_start:{name: "Gentle Pulses", sequence: [0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5]},
+    endurance_squeeze:{name: "Endurance Squeeze", sequence: [15,1,15]},
+    front_hold:{name: "Front Hold", sequence: [1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,]},
 
-        name: "on off",
-
-        sequence: [1,1,1,1]
-
-    },
-
-    rest: {
-
-        name: "Rest",
-
-        sequence: [0,3]
-
-    },
-
-    focus: {
-
-        name: "Focus",
-
-        sequence: [10,5,10,5]
-
-    }
 
 };
