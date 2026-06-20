8d6190d2193d7a301368a6f517d1de6706f471e4
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-19 19:26:51 -0700

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,27 @@
+const PROFILES = {
+
+    relax: {
+
+        name: "Relax",
+
+        sequence: [5,3,5,3,5,10]
+
+    },
+
+    box: {
+
+        name: "Box",
+
+        sequence: [4,4,4,4]
+
+    },
+
+    focus: {
+
+        name: "Focus",
+
+        sequence: [10,5,10,5]
+
+    }
+
+};
