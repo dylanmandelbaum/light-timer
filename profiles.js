3f08ae3cd6abda88b5ffb276ec3ffdff0bf94404
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-20 00:19:49 -0700

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -4,10 +4,12 @@ const PROFILES = {
     rest:{name: "Rest", sequence: [0,10]},
     complete:{name: "Complete!", sequence: [0,3]},
     gentle_pulses:{name: "Gentle Pulses", sequence: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
-    gentle_squeeze:{name: "Gentle Pulses", sequence: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]},
+    gentle_squeeze:{name: "Gentle Squeeze", sequence: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]},
   //  quick_start:{name: "Gentle Pulses", sequence: [0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5]},
+    quick_start_app_safe:{name: "Quick Start", sequence: [1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2]},
     endurance_squeeze:{name: "Endurance Squeeze", sequence: [15,1,15,1]},
  //   front_hold:{name: "Front Hold", sequence: [1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5]},
+    front_hold_app_safe:{name: "Front Hold", sequence: [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1]},    
     endurance_hold:{name: "Endurance Hold", sequence: [5,5,5,5,5,5,5,5,5,5]},
     core_beat:{name: "Core Beat", sequence: [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]},
     gradual_rise:{name: "Gradual Rise", sequence: [1,2,3,2,5,2,6,2,7,2,1,2,3,2,5,2,6,2,7,2]}
