bf5947d9000ace4508355312c356b2d13a475580
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-19 19:27:35 -0700

    Create patterns.js

---
+++ patterns.js
@@ -0,0 +1,26 @@
+const PATTERNS = {
+
+    morning: {
+
+        name: "Morning",
+
+        profiles: [
+            "relax",
+            "box",
+            "focus"
+        ]
+
+    },
+
+    sleep: {
+
+        name: "Sleep",
+
+        profiles: [
+            "box",
+            "relax"
+        ]
+
+    }
+
+};
