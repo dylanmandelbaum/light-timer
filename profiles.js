ee257b97d70c1395522cd7397fefd48576fc035d
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-20 19:24:09 -0700

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -5,7 +5,7 @@ const PROFILES = {
     complete:{name: "Complete!", sequence: [0,3]},
     gentle_pulses:{name: "Gentle Pulses", sequence: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
     gentle_squeeze:{name: "Gentle Squeeze", sequence: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]},
-    quick_start:{name: "Gentle Pulses", sequence: [0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5]},
+    quick_start:{name: "Quick Start", sequence: [0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5]},
     quick_start_app_safe:{name: "Quick Start", sequence: [1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2]},
     endurance_squeeze:{name: "Endurance Squeeze", sequence: [15,1,15,1]},
     front_hold:{name: "Front Hold", sequence: [1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5]},
