8599734df7a8a6dc7dcdd42a266b1a9011126c67
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-20 22:06:00 -0700

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -30,12 +30,153 @@ Tuesday: {
         name: "Tuesday",
 
         profiles: [
-                "slow_steps",
+                "get_ready",
+                "back_hold",
+                "rest",
                 "climb_pulse",
+                "rest",
+                "mid_hold",
+                "rest",
+                "slow_steps",
+                "rest",
+                "rapid_beats",
+                "rest",
+                "calm_wave",
+                "rest",
+                "snap_hold",
+                "rest",
+                "endurance_squeeze",
+                "complete",
         ]
 
     },
+    
+Wednesday: {
 
+        name: "Wednesday",
+
+        profiles: [
+                "get_ready",
+                "core_beat",
+                "rest",
+                "quick_start",
+                "rest",
+                "climb_pulse",
+                "rest",
+                "gentle_squeeze",
+                "rest",
+                "snap_hold",
+                "rest",
+                "gradual_rise",
+                "rest",
+                "back_hold",
+                "rest",
+                "endurance_hold",
+                "complete",
+        ]
+
+    },
+    
+    Thursday: {
+
+        name: "Thursday",
+
+        profiles: [
+                "get_ready",
+                "gentle_pulses",
+                "rest",
+                "slow_steps",
+                "rest",
+                "back_hold",
+                "rest",
+                "calm_wave",
+                "rest",
+                "front_hold",
+                "rest",
+                "endurance_squeeze",
+                "rest",
+                "front_hold",
+                "rest",
+                "slow_steps",
+                "complete",
+        ]
+
+    },
+    Friday: {
+
+        name: "Friday",
+
+        profiles: [
+                "get_ready",
+                "mid_hold",
+                "rest",
+                "quick_start",
+                "rest",
+                "back_hold",
+                "rest",
+                "endurance_squeeze",
+                "rest",
+                "core_beat",
+                "rest",
+                "gradual_rise",
+                "rest",
+                "front_hold",
+                "rest",
+                "endurance_hold",
+                "complete",
+        ]
+
+    },
+    Saturday: {
+
+        name: "Saturday",
+
+        profiles: [
+                "get_ready",
+                "rapid_beats",
+                "rest",
+                "mid_hold",
+                "rest",
+                "slow_steps",
+                "rest",
+                "front_hold",
+                "rest",
+                "climb_pulse",
+                "rest",
+                "back_hold",
+                "rest",
+                "snap_hold",
+                "rest",
+                "calm_wave",
+                "complete",
+        ]
+
+    },
+    Sunday: {
+
+        name: "Sunday",
+
+        profiles: [
+                "get_ready",
+                "gentle_pulses",
+                "rest",
+                "quick_start",
+                "rest",
+                "back_hold",
+                "rest",
+                "endurance_squeeze",
+                "rest",
+                "climb_pulse",
+                "rest",
+                "gradual_rise",
+                "rest",
+                "core_beat",
+                "rest",
+                "calm_wave",
+                "complete",
+        ]
+
+    },
 
 
 };
