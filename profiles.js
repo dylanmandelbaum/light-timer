f1f2a17bc00ba36d3ad8015197ac1e1eb68c0922
dylanmandelbaum <dylanmandelbaum@gmail.com>
2026-06-20 21:32:25 -0700

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -6,12 +6,19 @@ const PROFILES = {
     gentle_pulses:{name: "Gentle Pulses", sequence: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
     gentle_squeeze:{name: "Gentle Squeeze", sequence: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]},
     quick_start:{name: "Quick Start", sequence: [0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5,0.5,0.5,3.5,1.5]},
-    quick_start_app_safe:{name: "Quick Start", sequence: [1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2,1,1,4,2]},
     endurance_squeeze:{name: "Endurance Squeeze", sequence: [15,1,15,1]},
     front_hold:{name: "Front Hold", sequence: [1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5,1.5,0.5]},
-    front_hold_app_safe:{name: "Front Hold", sequence: [2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1]},    
+    mid_hold:{name: "Mid Hold", sequence: [3.5,3.5,3.5,3.5,3.5,3.5,3.5,3.5]},
+    back_hold:{name: "Back Hold", sequence: [2,0.5,2,0.5,2,0.5,2,0.5,2,0.5,2,0.5,2,0.5,2,0.5,2,0.5,2,0.5]}, 
     endurance_hold:{name: "Endurance Hold", sequence: [5,5,5,5,5,5,5,5,5,5]},
     core_beat:{name: "Core Beat", sequence: [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]},
-    gradual_rise:{name: "Gradual Rise", sequence: [1,2,3,2,5,2,6,2,7,2,1,2,3,2,5,2,6,2,7,2]}
+    gradual_rise:{name: "Gradual Rise", sequence: [1,2,3,2,5,2,6,2,7,2,1,2,3,2,5,2,6,2,7,2]},
+    climb_pulse:{name: "Climb Pulse (50%, 100%)", sequence: [1,0.25,2,1,1,0.25,2,1,1,0.25,2,1,1,0.25,2,1,1,0.25,2,1]},
+    slow_steps:{name: "Slow Steps (25%, 50%, 75%, 100%)", sequence: [3,0.25,3,0.25,3,0.25,3,3,3,0.25,3,0.25,3,0.25,3,3,3,0.25,3,0.25,3,0.25,3,3,3,0.25,3,0.25,3,0.25,3,3]},
+    rapid_beats:{name: "Rapid Beats", sequence: [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]},
+    calm_wave:{name: "Calm Wave", sequence: [5,3,5,3,5,3,5,3,5,3,5,3]},
+    snap_hold:{name: "Snap Hold", sequence: [1,0.5,2,0.5,1,0.5,2,0.5,1,0.5,2,0.5,1,0.5,2,0.5,1,0.5,2,0.5,1,0.5,2,0.5]},
+        
+    
     
 };
